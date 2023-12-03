b26153ce8bbe5c0199b59d3e31c263b68cca73f3
Khôi Nguyễn <originalkhoi151@gmail.com>
2023-12-03 12:01:32 +0700

    create users collection

---
+++ models/users.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const generate = require('../helpers/generateString');
+
+const usersSchema = new mongoose.Schema(
+{
+  fullName: String, 
+  email: String,
+  password: String,
+  tokenUser: {
+    type: String,
+    default: generate.generateRandomString(30)
+  },
+  phone: String,
+  avatar: String,
+  status: {
+    type: String,
+    default: 'active'
+  },
+  deleted: {
+    type: Boolean,
+    default: false
+  },
+  deletedAt: Date
+},
+{ timestamps: true })
+
+const Users = mongoose.model("Users", usersSchema, 'users');
+
+module.exports = Users;
