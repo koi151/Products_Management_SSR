d63c78d2dd5735658516c08b175c800cfd5bcd0c
Khôi Nguyễn <originalkhoi151@gmail.com>
2023-12-05 11:52:38 +0700

    add emoji for chat feature

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,5 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
+
 // CLIENT SEND MESSAGE
 const formDataSend = document.querySelector(".chat .inner-form");
 formDataSend.addEventListener('submit', (e) => {
@@ -29,8 +31,36 @@ socket.on('SERVER_RETURN_MESSAGE', (data) => {
     <div class='inner-content'>${data.content}</div>
   `
   chatBody.appendChild(newDiv);
+  chatBody.scrollTop = chatBody.scrollHeight;
 })
 
+// Scroll to newest chat
+const chatBody = document.querySelector('.chat .inner-body');
+if (chatBody) {
+  chatBody.scrollTop = chatBody.scrollHeight;
+}
 
+// Imoji picker
+const buttonIcon = document.querySelector('.button-icon');
+console.log('buttonIcon', buttonIcon)
+if (buttonIcon) {
+  const tooltip = document.querySelector('.tooltip');
+  console.log('tooltip', tooltip)
+  Popper.createPopper(buttonIcon, tooltip);
+  buttonIcon.onclick = () => {
+    tooltip.classList.toggle('shown')
+  }
+}
 
+const emojiPicker = document.querySelector('emoji-picker')
+if (emojiPicker) {
+  emojiPicker.addEventListener('emoji-click', event => {
+    const icon = event.detail.unicode; 
+    console.log(icon)
+    const inputChat = document.querySelector('.chat .inner-form input[name="content"]')
+    inputChat.value = inputChat.value + icon;
+    console.log('inputChat', inputChat)
+  });
+}
 
+  // .addEventListener('emoji-click', event => console.log(event.detail));
