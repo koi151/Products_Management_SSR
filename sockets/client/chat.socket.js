e4c92293941ce7d435c9b74d4a19d92dcff9f99a
Khôi Nguyễn <originalkhoi151@gmail.com>
2023-12-06 11:21:44 +0700

    update upload multiple images features

---
+++ sockets/client/chat.socket.js
@@ -0,0 +1,50 @@
+const Chat = require('../../models/chat.model');
+
+const uploadToCloudinary = require('../../helpers/uploadToCloudinary');
+
+module.exports = async (res) => {
+  const userId = res.locals.user.id;
+  const fullName = res.locals.user.fullName;
+
+  _io.once('connection', (socket) => {
+    socket.on('CLIENT_SEND_MESSAGE', async (data) => {
+
+      console.log('data image:', data.images)
+      let images = []
+      if (data.images) {
+        for (const imageBuffer of data.images) {
+          const link = await uploadToCloudinary(imageBuffer);
+          images.push(link);
+        }
+      }
+
+      console.log('images db', images)
+      // Save to database
+      const chat = new Chat({
+        user_id: userId,
+        content: data.content,
+        images: images
+      })
+
+      await chat.save();
+
+      // Return data to client
+      _io.emit('SERVER_RETURN_MESSAGE', {
+        userId: userId,
+        fullName: fullName,
+        content: data.content,
+        images: images
+      })
+    })
+
+    // Return data to client while client typing
+    socket.on('CLIENT_SEND_TYPING', (type) => {
+      console.log('userId sending:', userId)
+      socket.broadcast.emit('SERVER_RETURN_TYPING', {
+        userId: userId,
+        fullName: fullName,
+        type: type
+      })
+    })
+  })
+}
