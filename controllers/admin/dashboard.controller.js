9ef1260b7bfdbea838b0dcc9f38cb1b33c494c1d
Khôi Nguyễn <originalkhoi151@gmail.com>
2023-12-08 12:15:12 +0700

    update dashboard page and create 404 page

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,6 +1,96 @@
+const ProductCategories = require('../../models/products-category.model');
+const Products = require('../../models/products.model');
+const Accounts = require('../../models/accounts.model');
+const Users = require('../../models/users.model');
+
 // [GET] /admin/dashboard
-module.exports.dashboard = (req, res) => {
+module.exports.dashboard = async (req, res) => {
+  const statistics = {
+    productCategories: {
+      total: 0,
+      active: 0,
+      inactive: 0
+    },
+    products: {
+      total: 0,
+      active: 0,
+      inactive: 0
+    },
+    adminAccounts: {
+      total: 0,
+      active: 0,
+      inactive: 0
+    },
+    clientAccounts: {
+      total: 0,
+      active: 0,
+      inactive: 0
+    },
+  }
+
+  // Product categories
+  statistics.productCategories.total = await ProductCategories.count({
+    deleted: false
+  });
+
+  statistics.productCategories.active = await ProductCategories.count({
+    status: 'active',
+    deleted: false
+  });
+
+  statistics.productCategories.inactive = await ProductCategories.count({
+    status: 'inactive',
+    deleted: false
+  });
+
+  // Products
+  statistics.products.total = await Products.count({
+    deleted: false
+  });
+
+  statistics.products.active = await Products.count({
+    status: 'active',
+    deleted: false
+  });
+
+  statistics.products.inactive = await Products.count({
+    status: 'inactive',
+    deleted: false
+  });
+
+    // Admin accounts
+    statistics.adminAccounts.total = await Accounts.count({
+      deleted: false
+    });
+  
+    statistics.adminAccounts.active = await Accounts.count({
+      status: 'active',
+      deleted: false
+    });
+  
+    statistics.adminAccounts.inactive = await Accounts.count({
+      status: 'inactive',
+      deleted: false
+    });
+
+  // Users - client
+  statistics.clientAccounts.total = await Users.count({
+    deleted: false
+  });
+
+  statistics.clientAccounts.active = await Users.count({
+    status: 'active',
+    deleted: false
+  });
+
+  statistics.clientAccounts.inactive = await Users.count({
+    status: 'inactive',
+    deleted: false
+  });
+
+
   res.render("admin/pages/dashboard/dashboard.pug", {
-    pageTitle: 'DashBoard'
+    pageTitle: 'DashBoard',
+    statistics: statistics
   })
 }
--- index.js
+++ index.js
@@ -58,6 +58,12 @@ app.locals.moment = moment;
 routeClient(app);
 adminRoute(app);
 
+app.get('*', (req, res) => {
+  res.render('client/pages/errors/404', {
+    pageTitle: '404 Not Found'
+  })
+})
+
 server.listen(port, () => {
   console.log(`App listening on port ${port}`);
 })
